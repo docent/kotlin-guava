d7357d121bb61a91628386b571b31c5c4e6545a2
docent <docent@gmail.com>
2014-09-07 12:44:56 +0200

    Add concurrent library on top of guava

---
+++ src/main/kotlin/org/github/docent/kguava/ext/concurrent.kt
@@ -0,0 +1,25 @@
+package org.github.docent.kguava.ext
+
+import com.google.common.util.concurrent.ListenableFuture
+import com.google.common.util.concurrent.ListeningExecutorService
+import java.util.concurrent.TimeUnit
+import com.google.common.util.concurrent.Futures
+import com.google.common.util.concurrent.ListeningScheduledExecutorService
+
+
+inline fun <I, O> ListenableFuture<I>.transform(executor: ListeningScheduledExecutorService,
+                                                delay: Long, unit: TimeUnit, function: (I) -> O) : ListenableFuture<O> {
+
+    return Futures.transform(this) { (input: I) ->
+        executor.schedule({ () : O -> function(input)}, delay, unit)
+    }
+}
+
+inline fun <I, O> ListenableFuture<I>.transform(executor: ListeningExecutorService,
+                                                function: (I) -> O) : ListenableFuture<O> {
+    return Futures.transform(this) { (input: I) ->
+        executor.submit {() : O -> function(input) }
+    }
+}
+
+
